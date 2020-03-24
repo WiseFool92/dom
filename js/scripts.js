0b14cb3c23f0514e0c1e9d255ba3e6e77c558a04
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-03-24 16:15:32 -0700

    id tags set, page is operational

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   $("button#hello").click(function() {
-    $("ul#user").prepend("<li>Hello!</li>");
-    $("ul#webpage").prepend("<li>Why hello there!</li>");
+    $("ul#hooman").prepend("<li>Hello!</li>");
+    $("ul#puter").prepend("<li>Why hello there!</li>");
   });
 
   $("button#goodbye").click(function() {
-    $("ul#user").prepend("<li>Goodbye!</li>")
-    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+    $("ul#hooman").prepend("<li>Goodbye!</li>")
+    $("ul#puter").prepend("<li>Goodbye, dear user!</li>");
   });
 
   $("button#stop").click(function() {
-    $("ul#user").prepend("<li>Stop copying me!</li>");
-    $("ul#webpage").prepend("<li>Pardon me. I meant no offense</li>");
+    $("ul#hooman").prepend("<li>Stop copying me!</li>");
+    $("ul#puter").prepend("<li>Pardon me. I meant no offense</li>");
   });
 });
